ec145db31b249814607959def639057f93d48528
Athirakrishn <athiraramakrishnan41020@gmail.com>
2025-09-17 22:41:59 +0530

    cart completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './Pages/Home'
+import Cart from './Pages/Cart'
+import PageNotFound from './Pages/PageNotFound'
+import View from './Pages/View'
+import Wishlist from './Pages/Wishlist'
+import Footer from './Components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/:id/view' element={<View/>}/>
+        <Route path='/wishlist' element={<Wishlist/>}/>
+        <Route path='/*' element={<PageNotFound/>}/>
+      </Routes>
+      <Footer/>
     </>
   )
 }
