166d9b41d54593a86cd024f8bccbb452b6bf2401
Athirakrishn <athiraramakrishnan41020@gmail.com>
2025-09-18 18:28:42 +0530

    final

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import cartStore from './redux/CartStore.js'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import cartStore from './redux/CartStore.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
